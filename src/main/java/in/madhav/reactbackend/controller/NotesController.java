95c5c8ebc8e8d101993c0d5fac6bd56a6233ff9a
Madhav Teli <mteli@entrata.com>
2022-01-31 08:40:04 +0530

    delete note functionality

--- src/main/java/in/madhav/reactbackend/controller/NotesController.java
+++ src/main/java/in/madhav/reactbackend/controller/NotesController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,4 +39,12 @@ public class NotesController {
 	public ResponseEntity<Note> readNote(@PathVariable int id) {
 		return new ResponseEntity<Note>(notesRepo.findById(id).get(), HttpStatus.OK);
 	}
+	
+	
+	@DeleteMapping("/note/{id}")
+	public ResponseEntity<HttpStatus> deleteNote( @PathVariable int id ) {
+		notesRepo.deleteById( id );
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+	}
+	
 }
