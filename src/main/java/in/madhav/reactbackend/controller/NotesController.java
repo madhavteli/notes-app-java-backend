cfb55bd7461e72600f9b125452a5637a240dff7d
Madhav Teli <mteli@entrata.com>
2022-01-31 21:00:44 +0530

    update note feature

--- src/main/java/in/madhav/reactbackend/controller/NotesController.java
+++ src/main/java/in/madhav/reactbackend/controller/NotesController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.madhav.reactbackend.entity.Note;
@@ -45,6 +47,11 @@ public class NotesController {
 	public ResponseEntity<HttpStatus> deleteNote( @PathVariable int id ) {
 		notesRepo.deleteById( id );
 		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping("/updateNote")
+	public ResponseEntity<Note> updateNote( @RequestBody Note note ) {
+		return new ResponseEntity<Note>(notesRepo.save(note), HttpStatus.OK);
 	}
 	
 }
